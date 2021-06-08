34f34c9b5df0ed159b1256db60ecfa98854a877a
Sujal Shah <sujalhshah@gmail.com>
2021-06-08 21:54:33 +1200

    Create Rest API for Insurance Policies

--- functions.php
+++ functions.php
@@ -626,3 +626,48 @@ function disable_autosave() {
   }
   add_action('wp_print_scripts','disable_autosave' );
   remove_action('pre_post_update','wp_save_post_revision' );
+
+
+//REST API for Policy
+add_action( 'rest_api_init', 'create_rest_api_policies' );   
+
+function custom_api_get_all_posts() {
+    register_rest_route( 'v2', '/policies', array(
+        'methods' => 'GET',
+        'callback' => 'rest_pi_get_all_policies_callback'
+    ));
+}
+
+function rest_pi_get_all_policies_callback( $request ) {
+    // Initialize the array that will receive the posts' data. 
+    $posts_data = array();
+
+    // Receive and set the page parameter from the $request for pagination purposes
+    $paged = $request->get_param( 'page' );
+    $paged = ( isset( $paged ) || ! ( empty( $paged ) ) ) ? $paged : 1; 
+
+    // Get the posts using the 'post' and 'news' post types
+    $posts = get_posts( array(
+            'paged' => $paged,
+            'posts_per_page' => 10,            
+            'post_type' => array( 'policy' ), // This is the line that allows to fetch multiple post types. 
+			'post_status'     => 'publish',
+        )
+    ); 
+
+    // Loop through the posts and push the desired data to the array we've initialized earlier in the form of an object
+    foreach( $posts as $post ) {
+        $id = $post->ID; 
+        $post_thumbnail = ( has_post_thumbnail( $id ) ) ? get_the_post_thumbnail_url( $id ) : null;
+
+        $posts_data[] = (object) array( 
+            'id' => $id, 
+            'url' => get_permalink($id),
+            'title' => get_post_meta($id, 'policy_name', true),
+			'policy_id' => get_post_meta($id, 'policy_id', true),
+			'live_date' => get_post_meta($id, 'live_date', true),
+			'description' => get_post_meta($id, 'description', true)
+        );
+    }                  
+    return $posts_data;                   
+}
